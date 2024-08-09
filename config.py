06d05d6af6e163c4075165e1cb85b73bf9b1f575
Yahyaraza <72003194+Yahyaraza@users.noreply.github.com>
2024-08-09 12:29:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7424479555:AAG21En_vWhghzaUAhKjJ-DoZaLV3KBH4")
+BOT_TOKEN=("7265908157:AAFpOkuWJX3zFvis148oCWhENLiEiGY0SNA")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("NOOBLOWNER")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("2030918750")
 
 
 
